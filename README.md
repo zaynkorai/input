e0346624288b1f5e7515a15b7b6f5e2014fcdec4
unknown <zaynulabdin313@gmail.com>
2019-08-19 15:17:34 +0500

    basics feature working

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# input
+# input50
 Input Library for Rust like CS50
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,95 @@
-mod tests {
+use std::io;
 
-    pub fn hello() -> String {
-        ("Hello, world!").to_string()
+mod Rust50 {
+
+    /**
+    * Prompts user for a line of text from standard input and returns the
+    * equivalent char; if text is not a single char, user is prompted
+    * to retry. If line can't be read, returns .
+    */
+    pub fn get_char() -> char 
+    {
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer.trim().parse::<char>().unwrap()
     }
 
-    pub fn get_char(){
-    
+    /**
+    * Prompts user for a line of text from standard input and returns the
+    * equivalent double as precisely as possible; if text does not represent
+    * a double or if value would cause underflow or overflow, user is
+    * prompted to retry. If line can't be read, returns .
+    */
+    pub fn get_double() -> f64 {
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer.trim().parse::<f64>().unwrap()
     }
-    pub fn get_double(){
-    
-    }
-    pub fn get_float(){
-    
-    }
-    pub fn get_int(){
-    
-    }
-    pub fn get_long(){
-    
-    }
-    pub fn get_string(){
-    
-}
 
-}
-#[cfg(test)]
-mod tests {
-
-    use super::input::hello;
-
-    #[test]
-    fn test_hello() {
-        assert_eq!(hello(), "Hello, world!");
+    /**
+    * Prompts user for a line of text from standard input and returns the
+    * equivalent float as precisely as possible; if text does not represent
+    * a float or if value would cause underflow or overflow, user is prompted
+    * to retry. If line can't be read, returns .
+    */
+    pub fn get_float() -> f32 {
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer.trim().parse::<f32>().unwrap()
     }
+
+    /**
+    * Prompts user for a line of text from standard input and returns the
+    * equivalent int; if text does not represent an int in [-2^31, 2^31 - 1)
+    * or would cause underflow or overflow, user is prompted to retry. If line
+    * can't be read, returns.
+    */
+    pub fn get_int() -> i32 {
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer.trim().parse::<i32>().unwrap()
+    }
+
+    /**
+    * Prompts user for a line of text from standard input and returns the
+    * equivalent long; if text does not represent a long in
+    * [-2^63, 2^63 - 1) or would cause underflow or overflow, user is
+    * prompted to retry. If line can't be read, returns.
+    */
+    pub fn get_long() -> i64 {
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer.trim().parse::<i64>().unwrap()
+    }
+
+    /**
+    * Prompts user for a line of text from standard input and returns
+    * it as a string, sans trailing line ending. Supports
+    * CR (\r), LF (\n), and CRLF (\r\n) as line endings. If user
+    * inputs only a line ending, returns "", not NULL. Returns NULL
+    * upon error or no input whatsoever (i.e., just EOF). Stores string
+    * on heap.
+    */
+    pub fn get_string()-> String {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer
+    }
+
+    /**
+    * Prompts user for a line of text from standard input with predefine text and returns
+    * it as a string, sans trailing line ending. Supports
+    * CR (\r), LF (\n), and CRLF (\r\n) as line endings. If user
+    * inputs only a line ending, returns "", not NULL. Returns NULL
+    * upon error or no input whatsoever (i.e., just EOF). Stores string
+    * on heap.
+    */
+
+    pub fn input(message:String) -> String {
+        print!("{} ", message);
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).expect("Failed to Input");
+        buffer
+    }
+
 }
